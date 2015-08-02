cc88471fd589fb521e3a2d2a23c4255a91b536fd
Digiapt <web.admin@digiapt.com>
2015-08-03 00:13:23 +0530

    fixed invariant.js

--- invariant.js
+++ invariant.js
@@ -22,8 +22,6 @@
  * will remain to ensure logic does not differ in production.
  */
 
-var __DEV__ = process.env.NODE_ENV !== 'production';
-
 var invariant = function(condition, format, a, b, c, d, e, f) {
   if (__DEV__) {
     if (format === undefined) {
